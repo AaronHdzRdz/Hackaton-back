7e141093e9074ac5c4094fc1613e91b59f5d13cf
OmarAguilar43 <omaroaguilar43@gmail.com>
2025-03-28 22:31:42 -0600

    Omar

--- src/models/pet.js
+++ src/models/pet.js
@@ -5,6 +5,7 @@ const petSchema = new mongoose.Schema({
   name: { type: String, required: true },
   species: { type: String, enum: ['Perro', 'Gato'], required: true },
   breed: String,
+  size: { type: String, enum: ['Pequeño', 'Mediano', 'Grande', 'Único'], required: true },
   age: Number,
   weight: Number,
   photoUrl: String,
